aec0236fbd8ab9111fbc7f00f9bca8a02f2f51ff
Nick Magee <NCM200001@utdallas.edu>
2021-10-30 17:49:40 -0500

    initial commit, p5 background

---
+++ p5-bg-more/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  var div = document.getElementById("sketch");
+  var width = div.offsetWidth;
+  var height = div.offsetHeight;
+
+  var myCanvas = createCanvas(width, height);
+  myCanvas.parent("sketch");
+}
+
+function draw() {
+  background(255, 0, 0);
+  fill(255, 0, 0);
+  //   rect;/
+}
