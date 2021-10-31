b0a0dd54dbe224ffcaebb9ae1b9e1e3da041eeae
Nick Magee <NCM200001@utdallas.edu>
2021-10-31 18:50:48 -0500

    react bg working!

--- react-component/square-nav-bg/src/nav-component/SquareNavigation.jsx
+++ react-component/square-nav-bg/src/nav-component/SquareNavigation.jsx
@@ -1,38 +1,89 @@
 import React, { useEffect, useState } from "react";
 import { ReactP5Wrapper } from "react-p5-wrapper";
 
-const SquareNavigation = ({ parentRef }) => {
-  const [circles, setCircles] = useState([]);
+class Circle {
+  constructor(
+    x,
+    y,
+    r,
+    sw,
+    x_speed,
+    y_speed,
+    num_rings,
+    spread_offset,
+    width,
+    height
+  ) {
+    // this.p5 = p5;
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.sw = sw;
+    this.x_s = x_speed;
+    this.y_s = y_speed;
+    this.num_rings = num_rings;
+    this.spread_offset = spread_offset;
+    this.width = width;
+    this.height = height;
+  }
 
-  useEffect(() => {
-    var CIRCLES_NUM = 5;
-    let MIN_R = 400;
-    let MAX_R = 500;
-    let MAX_SPEED = 1;
-    let circles_local = [];
-    for (let i = 0; i < CIRCLES_NUM; i++) {
-      //   let
-      let x = getRandomNumberBetween(0, 500);
-      let y = getRandomNumberBetween(0, 500);
-      let r = getRandomNumberBetween(MIN_R, MAX_R);
-      let stroke_weight = getRandomNumberBetween(1, 7);
-      let x_speed = getRandomNumberBetween(-MAX_SPEED, MAX_SPEED);
-      let y_speed = getRandomNumberBetween(-MAX_SPEED, MAX_SPEED);
-      let num_rings = Math.floor(getRandomNumberBetween(1, 3));
-      let spread_offset = getRandomNumberBetween(20, 40);
-      circles_local[i] = new Circle(
-        x,
-        y,
-        r,
-        stroke_weight,
-        x_speed,
-        y_speed,
-        num_rings,
-        spread_offset
-      );
+  run() {
+    // this.p5.noFill();
+    // this.p5.stroke(255);
+    // this.p5.strokeWeight(this.sw);
+    // this.p5.ellipse(this.x, this.y, this.r);
+    // if (this.num_rings === 2) {
+    //   this.p5.ellipse(this.x, this.y, this.r + this.spread_offset);
+    // }
+    this.x += this.x_s;
+    this.y += this.y_s;
+
+    if (this.x > this.width) {
+      this.x_s = -this.x_s;
     }
-    setCircles(circles_local);
-  }, []);
+    if (this.x < 0) {
+      this.x_s = -this.x_s;
+    }
+    if (this.y > this.height) {
+      this.y_s = -this.y_s;
+    }
+    if (this.y < 0) {
+      this.y_s = -this.y_s;
+    }
+  }
+}
+
+var CIRCLES_NUM = 8;
+let MIN_R = 40;
+let MAX_R = 200;
+let MAX_SPEED = 1;
+let circles = [];
+for (let i = 0; i < CIRCLES_NUM; i++) {
+  //   let
+  let x = getRandomNumberBetween(0, window.innerWidth);
+  let y = getRandomNumberBetween(0, window.innerHeight);
+  let r = getRandomNumberBetween(MIN_R, MAX_R);
+  let stroke_weight = getRandomNumberBetween(1, 7);
+  let x_speed = getRandomNumberBetween(-MAX_SPEED, MAX_SPEED);
+  let y_speed = getRandomNumberBetween(-MAX_SPEED, MAX_SPEED);
+  let num_rings = Math.floor(getRandomNumberBetween(1, 3));
+  let spread_offset = getRandomNumberBetween(20, 40);
+  circles[i] = new Circle(
+    x,
+    y,
+    r,
+    stroke_weight,
+    x_speed,
+    y_speed,
+    num_rings,
+    spread_offset,
+    window.innerWidth,
+    window.innerHeight
+  );
+}
+
+const SquareNavigation = ({ parentRef }) => {
+  // const [circles, setCircles] = useState([]);
 
   const sketch = (p5) => {
     p5.setup = () => {
@@ -45,15 +96,27 @@ const SquareNavigation = ({ parentRef }) => {
       //   p5.fill(255, 0, 0);
       //   p5.rect(0, 0, 50, 50);
       for (let i = 0; i < circles.length; i++) {
-        // circles[i].run();
+        circles[i].run();
         p5.noFill();
         p5.stroke(255);
+        p5.strokeWeight(circles[i].sw);
+        if (circles[i].num_rings === 2) {
+          p5.ellipse(
+            circles[i].x,
+            circles[i].y,
+            circles[i].r + circles[i].spread_offset
+          );
+        }
         p5.ellipse(circles[i].x, circles[i].y, circles[i].r);
       }
     };
 
     p5.windowResized = () => {
       p5.resizeCanvas(window.innerWidth, window.innerHeight);
+      for (let i = 0; i < circles.length; i++) {
+        circles[i].width = window.innerWidth;
+        circles[i].height = window.innerHeight;
+      }
     };
   };
 
@@ -62,49 +125,10 @@ const SquareNavigation = ({ parentRef }) => {
 
 export default SquareNavigation;
 
-class Circle {
-  constructor(p5, x, y, r, sw, x_speed, y_speed, num_rings, spread_offset) {
-    this.p5 = p5;
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.sw = sw;
-    this.x_s = x_speed;
-    this.y_s = y_speed;
-    this.num_rings = num_rings;
-    this.spread_offset = spread_offset;
-  }
-
-  run() {
-    this.p5.noFill();
-    this.p5.stroke(255);
-    this.p5.strokeWeight(this.sw);
-    this.p5.ellipse(this.x, this.y, this.r);
-    if (this.num_rings === 2) {
-      this.p5.ellipse(this.x, this.y, this.r + this.spread_offset);
-    }
-    this.x += this.x_s;
-    this.y += this.y_s;
-
-    if (this.x > this.p5.width) {
-      this.x_s = -this.x_s;
-    }
-    if (this.x < 0) {
-      this.x_s = -this.x_s;
-    }
-    if (this.y > this.p5.height) {
-      this.y_s = -this.y_s;
-    }
-    if (this.y < 0) {
-      this.y_s = -this.y_s;
-    }
-  }
-}
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function getRandomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.random() * (max - min + 1) + min;
 }
